b6927c9e6bd8213a42d13563d99df6d9cd76adc6
xhide341 <shawnehgn10@gmail.com>
2025-12-03 00:43:23 +0800

    style: add color theme for auth buttons

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -88,7 +88,7 @@ export default function Login({
 
                             <Button
                                 type="submit"
-                                className="mt-4 w-full"
+                                className="mt-4 w-full bg-[#593A2F] hover:bg-[#4a2f26]"
                                 tabIndex={4}
                                 disabled={processing}
                                 data-test="login-button"
--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -92,7 +92,7 @@ export default function Register() {
 
                             <Button
                                 type="submit"
-                                className="mt-2 w-full"
+                                className="mt-2 w-full bg-[#593A2F] hover:bg-[#4a2f26]"
                                 tabIndex={5}
                                 data-test="register-user-button"
                             >
